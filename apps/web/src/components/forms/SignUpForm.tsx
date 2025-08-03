5ccf8c15a4b4a18f555a47482ad6eef4f0a38e1f
Edrian Sulague <edrianghamaliel.sulague@gmail.com>
2025-08-04 00:04:33 +0800

    new signin and signout

--- apps/web/src/components/forms/SignUpForm.tsx
+++ apps/web/src/components/forms/SignUpForm.tsx
@@ -3,7 +3,9 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { supabase } from "@/lib/supabase/client";
-import { Eye, EyeOff } from "lucide-react";
+import { Eye, EyeOff, Loader2 } from "lucide-react";
+import Image from "next/image";
+import { toast } from "sonner";
 
 export default function Signup() {
   const [email, setEmail] = useState("");
@@ -12,80 +14,127 @@ export default function Signup() {
   const [showPassword, setShowPassword] = useState(false);
   const [showConfirm, setShowConfirm] = useState(false);
   const router = useRouter();
+  const [loading, setLoading] = useState(false);
 
   const handleSignUp = async () => {
+    setLoading(true);
+
+    const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
+    if (!email || !password || !confirmPassword) {
+      await delay(500); // 0.5 second delay before hiding loader
+      setLoading(false);
+      toast("Please fill in all fields.");
+      return;
+    }
+
     if (password !== confirmPassword) {
-      alert("Passwords do not match.");
+      await delay(500); // 0.5 second delay before hiding loader
+      setLoading(false);
+      toast("Passwords do not match.");
       return;
     }
 
     const { error } = await supabase.auth.signUp({ email, password });
-    if (error) alert(error.message);
-    else {
-      alert("Check your email for confirmation.");
+
+    setLoading(false); // Always set after async op
+
+    if (error) {
+      toast(error.message);
+    } else {
+      toast("Check your email for confirmation.");
       router.push("/signin");
     }
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-white px-4">
-      <div className="w-full max-w-md bg-white border border-gray-200 rounded-xl p-8 shadow-md">
-        <h2 className="text-2xl font-semibold text-gray-800 mb-6 text-center">
-          Sign Up
-        </h2>
-        <div className="space-y-4">
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder="Email"
-            className="w-full px-4 py-3 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500  text-gray-800 placeholder-gray-400"
-          />
-          <div className="relative">
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="w-full max-w-5xl md:h-[480px] bg-white rounded-lg shadow-lg grid grid-cols-1 md:grid-cols-2 overflow-hidden">
+        <div className="w-full max-w-5xl p-10">
+          <h2 className="text-2xl font-bold text-gray-800 mb-6 text-center">
+            SIGN UP
+          </h2>
+          <div className="space-y-4">
+            <label className="block text-sm font-medium text-gray-700 mb-1">
+              Email
+            </label>
             <input
-              type={showPassword ? "text" : "password"}
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="Password"
-              className="w-full px-4 py-3 pr-12 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500  text-gray-800 placeholder-gray-400"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email"
+              className="w-full px-4 py-3 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500  text-gray-800 placeholder-gray-400"
             />
+            <div className="relative">
+              <label className="block text-sm font-medium text-gray-700 mb-1">
+                Password
+              </label>
+              <input
+                type={showPassword ? "text" : "password"}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Password"
+                className="w-full px-4 py-3 pr-12 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500  text-gray-800 placeholder-gray-400"
+              />
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute inset-y-12 right-3 flex items-center text-gray-500"
+              >
+                {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+              </button>
+            </div>
+            <div className="relative">
+              <label className="block text-sm font-medium text-gray-700 mb-1">
+                Confirm Password
+              </label>
+              <input
+                type={showConfirm ? "text" : "password"}
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                placeholder="Confirm Password"
+                className="w-full px-4 py-3 pr-12 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-800"
+              />
+              <button
+                type="button"
+                onClick={() => setShowConfirm(!showConfirm)}
+                className="absolute inset-y-12 right-3 flex items-center text-gray-500"
+              >
+                {showConfirm ? <EyeOff size={20} /> : <Eye size={20} />}
+              </button>
+            </div>
             <button
-              type="button"
-              onClick={() => setShowPassword(!showPassword)}
-              className="absolute inset-y-0 right-3 flex items-center text-gray-500"
+              onClick={handleSignUp}
+              disabled={loading}
+              className="w-full py-3 bg-green-800 hover:bg-green-900 text-white font-medium rounded-md flex items-center justify-center disabled:opacity-50 disabled:cursor-not-allowed"
             >
-              {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+              {loading ? (
+                <>
+                  <Loader2 className="animate-spin mr-2 h-5 w-5" />
+                  Signing up...
+                </>
+              ) : (
+                "Sign Up"
+              )}
             </button>
           </div>
-          <div className="relative">
-            <input
-              type={showConfirm ? "text" : "password"}
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              placeholder="Confirm Password"
-              className="w-full px-4 py-3 pr-12 rounded-md border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 text-gray-800"
-            />
-            <button
-              type="button"
-              onClick={() => setShowConfirm(!showConfirm)}
-              className="absolute inset-y-0 right-3 flex items-center text-gray-500"
-            >
-              {showConfirm ? <EyeOff size={20} /> : <Eye size={20} />}
-            </button>
-          </div>
-          <button
-            onClick={handleSignUp}
-            className="w-full py-3 bg-green-600 hover:bg-green-700 transition rounded-md text-white font-medium"
-          >
-            Sign Up
-          </button>
+          <p className="mt-6 text-center text-sm text-gray-600">
+            Already have an account?{" "}
+            <a href="/signin" className="text-blue-600 hover:underline">
+              Sign in
+            </a>
+          </p>
         </div>
-        <p className="mt-6 text-center text-sm text-gray-600">
-          Already have an account?{" "}
-          <a href="/signin" className="text-blue-600 hover:underline">
-            Sign in
-          </a>
-        </p>
+
+        <div className="w-full h-full">
+          <Image
+            src="/BACKGROUND.png"
+            alt="Background"
+            width={1000}
+            height={1000}
+            className="w-full h-full object-fill"
+          />
+        </div>
       </div>
     </div>
   );
